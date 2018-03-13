f735d502658510b25056fc63dabd74e2a12936e6
mayurdhvajsinh-gohil41 <mayurdhvajsinh.gohil@einfochips.com>
2018-03-13 12:33:52 +0530

    my first commit

---
+++ Example.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+class Example
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        int a, b, total;
+        System.out.println("Enter first number");
+        a = scan.nextInt();
+        System.out.println("Enter second number");
+        b = scan.nextInt();
+        total = sum(a,b);
+        System.out.println("Sum of two integer is: "+total);
+    }
+
+    public static int sum(int a, int b)
+    {
+        int total;
+        total = a + b;
+        return total;
+    }
+}
